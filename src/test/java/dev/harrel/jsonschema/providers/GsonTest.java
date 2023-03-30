66a919e6fa03c2202c605d300ef56294d387f047
Harrel <marcin.slowik95@gmail.com>
2023-03-30 21:44:58 +0200

    JsonNodeFactory wrap tests

--- src/test/java/dev/harrel/jsonschema/providers/GsonTest.java
+++ src/test/java/dev/harrel/jsonschema/providers/GsonTest.java
@@ -1,9 +1,35 @@
 package dev.harrel.jsonschema.providers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import dev.harrel.jsonschema.JsonNode;
+import dev.harrel.jsonschema.SimpleType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GsonTest {
+    @Test
+    void shouldWrapForValidArgument() throws JsonProcessingException {
+        JsonElement object = JsonParser.parseString("{}");
+        JsonNode wrap = new GsonNode.Factory().wrap(object);
+        assertThat(wrap).isNotNull();
+        assertThat(wrap.getNodeType()).isEqualTo(SimpleType.OBJECT);
+    }
+
+    @Test
+    void shouldFailWrapForInvalidArgument() throws JsonProcessingException {
+        com.fasterxml.jackson.databind.JsonNode object = new ObjectMapper().readTree("{}");
+        GsonNode.Factory factory = new GsonNode.Factory();
+        assertThatThrownBy(() -> factory.wrap(object))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Nested
     class SpecificationTest extends dev.harrel.jsonschema.SpecificationTest {
         @BeforeAll
--- src/test/java/dev/harrel/jsonschema/providers/JacksonTest.java
+++ src/test/java/dev/harrel/jsonschema/providers/JacksonTest.java
@@ -1,9 +1,33 @@
 package dev.harrel.jsonschema.providers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.harrel.jsonschema.SimpleType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class JacksonTest {
+    @Test
+    void shouldWrapForValidArgument() throws JsonProcessingException {
+        JsonNode object = new ObjectMapper().readTree("{}");
+        JacksonNode wrap = new JacksonNode.Factory().wrap(object);
+        assertThat(wrap).isNotNull();
+        assertThat(wrap.getNodeType()).isEqualTo(SimpleType.OBJECT);
+    }
+
+    @Test
+    void shouldFailWrapForInvalidArgument() throws JsonProcessingException {
+        Integer object = new ObjectMapper().readValue("1", Integer.class);
+        JacksonNode.Factory factory = new JacksonNode.Factory();
+        assertThatThrownBy(() -> factory.wrap(object))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Nested
     class SpecificationTest extends dev.harrel.jsonschema.SpecificationTest {
         @BeforeAll
