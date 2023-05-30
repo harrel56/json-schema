816a1f4ccd0793f42038990b2c94a1b662f2c8e0
Harrel <marcin.slowik95@gmail.com>
2023-05-30 22:13:30 +0200

    more ValidatorFactory tests

--- src/test/java/dev/harrel/jsonschema/ValidatorFactoryTest.java
+++ src/test/java/dev/harrel/jsonschema/ValidatorFactoryTest.java
@@ -1,5 +1,8 @@
 package dev.harrel.jsonschema;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.harrel.jsonschema.providers.JacksonNode;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,6 +13,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidatorFactoryTest {
+    private static final String RAW_SCHEMA = "{\"type\":\"boolean\"}";
+    private static final String RAW_INSTANCE = "null";
 
     @Test
     void name() {
@@ -43,5 +48,71 @@ class ValidatorFactoryTest {
                 .createValidator();
         assertThatThrownBy(() -> validator.registerSchema(schema))
                 .isInstanceOf(MetaSchemaResolvingException.class);
+    }
+
+    @Test
+    void validateStringString() {
+        boolean valid = new ValidatorFactory().validate(RAW_SCHEMA, RAW_INSTANCE).isValid();
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void validateProviderNodeString() throws JsonProcessingException {
+        Object providerNode = new ObjectMapper().readTree(RAW_SCHEMA);
+        boolean valid = new ValidatorFactory().validate(providerNode, RAW_INSTANCE).isValid();
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void validateJsonNodeString() {
+        JsonNode jsonNode = new JacksonNode.Factory().create(RAW_SCHEMA);
+        boolean valid = new ValidatorFactory().validate(jsonNode, RAW_INSTANCE).isValid();
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void validateStringProviderNode() throws JsonProcessingException {
+        Object providerNode = new ObjectMapper().readTree(RAW_INSTANCE);
+        boolean valid = new ValidatorFactory().validate(RAW_SCHEMA, providerNode).isValid();
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void validateProviderNodeProviderNode() throws JsonProcessingException {
+        Object providerNodeSchema = new ObjectMapper().readTree(RAW_SCHEMA);
+        Object providerNodeInstance = new ObjectMapper().readTree(RAW_INSTANCE);
+        boolean valid = new ValidatorFactory().validate(providerNodeSchema, providerNodeInstance).isValid();
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void validateJsonNodeProviderNode() throws JsonProcessingException {
+        JsonNode jsonNode = new JacksonNode.Factory().create(RAW_SCHEMA);
+        Object providerNode = new ObjectMapper().readTree(RAW_INSTANCE);
+        boolean valid = new ValidatorFactory().validate(jsonNode, providerNode).isValid();
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void validateStringJsonNode() {
+        JsonNode jsonNode = new JacksonNode.Factory().create(RAW_INSTANCE);
+        boolean valid = new ValidatorFactory().validate(RAW_SCHEMA, jsonNode).isValid();
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void validateProviderNodeJsonNode() throws JsonProcessingException {
+        Object providerNode = new ObjectMapper().readTree(RAW_SCHEMA);
+        JsonNode jsonNode = new JacksonNode.Factory().create(RAW_INSTANCE);
+        boolean valid = new ValidatorFactory().validate(providerNode, jsonNode).isValid();
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void validateJsonNodeJsonNode() {
+        JsonNode jsonNodeSchema = new JacksonNode.Factory().create(RAW_SCHEMA);
+        JsonNode jsonNodeInstance = new JacksonNode.Factory().create(RAW_INSTANCE);
+        boolean valid = new ValidatorFactory().validate(jsonNodeSchema, jsonNodeInstance).isValid();
+        assertThat(valid).isFalse();
     }
 }
