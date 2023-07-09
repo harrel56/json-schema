1a2a5b07c68784b73039948d14df1618e4b5efac
harrel56 <marcin.slowik95@gmail.com>
2023-07-09 13:24:25 +0200

    test: add NoOpMetaSchemaValidatorTest

--- src/main/java/dev/harrel/jsonschema/VocabularyException.java
+++ src/main/java/dev/harrel/jsonschema/VocabularyException.java
@@ -1,5 +1,8 @@
 package dev.harrel.jsonschema;
 
+/**
+ * Exception type used to indicate vocabulary related issues.
+ */
 public class VocabularyException extends JsonSchemaException {
     VocabularyException(String message) {
         super(message);
