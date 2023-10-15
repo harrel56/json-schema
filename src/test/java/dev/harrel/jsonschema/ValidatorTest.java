3657039814f76c0972d28a9835a81801e6f43100
Harrel <marcin.slowik95@gmail.com>
2023-10-15 15:36:01 +0200

    expand tests with enabled schema validation

--- src/test/java/dev/harrel/jsonschema/ValidatorTest.java
+++ src/test/java/dev/harrel/jsonschema/ValidatorTest.java
@@ -186,6 +186,7 @@ class ValidatorTest {
 
     @Test
     void allowsEmptyFragmentsInIdRootSchema() {
+        // with disabled schema validation
         Validator validator = new ValidatorFactory()
                 .withDisabledSchemaValidation(true)
                 .createValidator();
@@ -197,10 +198,18 @@ class ValidatorTest {
         URI uri = validator.registerSchema(schema);
         Validator.Result result = validator.validate(uri, "true");
         assertThat(result.isValid()).isTrue();
+
+        // with enabled schema validation
+        validator = new ValidatorFactory().createValidator();
+
+        uri = validator.registerSchema(schema);
+        result = validator.validate(uri, "true");
+        assertThat(result.isValid()).isTrue();
     }
 
     @Test
     void disallowsNonEmptyFragmentsInIdRootSchema() {
+        // with disabled schema validation
         Validator validator = new ValidatorFactory()
                 .withDisabledSchemaValidation(true)
                 .createValidator();
@@ -220,10 +229,22 @@ class ValidatorTest {
         assertThatThrownBy(() -> validator.registerSchema(schema2))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("$id [urn:test#/$defs/x] cannot contain non-empty fragments");
+
+        // with enabled schema validation
+        Validator validator2 = new ValidatorFactory().createValidator();
+
+        assertThatThrownBy(() -> validator2.registerSchema(schema1))
+                .isInstanceOf(InvalidSchemaException.class)
+                .hasMessage("Schema [urn:test#anchor] failed to validate against meta-schema [https://json-schema.org/draft/2020-12/schema]");
+
+        assertThatThrownBy(() -> validator2.registerSchema(schema2))
+                .isInstanceOf(InvalidSchemaException.class)
+                .hasMessage("Schema [urn:test#/$defs/x] failed to validate against meta-schema [https://json-schema.org/draft/2020-12/schema]");
     }
 
     @Test
     void allowsEmptyFragmentsInIdSubSchema() {
+        // with disabled schema validation
         Validator validator = new ValidatorFactory()
                 .withDisabledSchemaValidation(true)
                 .createValidator();
@@ -239,10 +260,18 @@ class ValidatorTest {
         URI uri = validator.registerSchema(schema);
         Validator.Result result = validator.validate(uri, "true");
         assertThat(result.isValid()).isTrue();
+
+        // with enabled schema validation
+        validator = new ValidatorFactory().createValidator();
+
+        uri = validator.registerSchema(schema);
+        result = validator.validate(uri, "true");
+        assertThat(result.isValid()).isTrue();
     }
 
     @Test
     void disallowsNonEmptyFragmentsInIdSubSchema() {
+        // with disabled schema validation
         Validator validator = new ValidatorFactory()
                 .withDisabledSchemaValidation(true)
                 .createValidator();
@@ -270,5 +299,17 @@ class ValidatorTest {
         assertThatThrownBy(() -> validator.registerSchema(schema2))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("$id [urn:sub#/$defs/x] cannot contain non-empty fragments");
+
+        // with enabled schema validation
+        Validator validator2 = new ValidatorFactory().createValidator();
+        URI uri = URI.create("urn:sub");
+
+        assertThatThrownBy(() -> validator2.registerSchema(uri, schema1))
+                .isInstanceOf(InvalidSchemaException.class)
+                .hasMessage("Schema [urn:sub] failed to validate against meta-schema [https://json-schema.org/draft/2020-12/schema]");
+
+        assertThatThrownBy(() -> validator2.registerSchema(uri, schema2))
+                .isInstanceOf(InvalidSchemaException.class)
+                .hasMessage("Schema [urn:sub] failed to validate against meta-schema [https://json-schema.org/draft/2020-12/schema]");
     }
 }
