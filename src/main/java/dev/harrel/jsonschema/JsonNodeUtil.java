f87c0e74147326646a61c046428d61465503ca08
Harrel <marcin.slowik95@gmail.com>
2023-10-18 22:39:55 +0200

    refactor: extract methods to JsonNodeUtil

--- src/main/java/dev/harrel/jsonschema/JsonNodeUtil.java
+++ src/main/java/dev/harrel/jsonschema/JsonNodeUtil.java
@@ -1,8 +1,10 @@
 package dev.harrel.jsonschema;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 final class JsonNodeUtil {
     private JsonNodeUtil() {}
@@ -15,6 +17,20 @@ final class JsonNodeUtil {
         return Optional.ofNullable(objectMap.get(fieldName))
                 .filter(JsonNode::isString)
                 .map(JsonNode::asString);
+    }
+
+    static Optional<Map<String, JsonNode>> getObjectField(Map<String, JsonNode> objectMap, String fieldName) {
+        return Optional.ofNullable(objectMap.get(fieldName))
+                .filter(JsonNode::isObject)
+                .map(JsonNode::asObject);
+    }
+
+    static Optional<Map<String, Boolean>> getVocabulariesObject(Map<String, JsonNode> objectNode) {
+        return getObjectField(objectNode, Keyword.VOCABULARY)
+                .map(obj -> obj.entrySet().stream()
+                        .filter(entry -> entry.getValue().isBoolean())
+                        .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().asBoolean())))
+                .map(Collections::unmodifiableMap);
     }
 
     static void validateIdField(String id) {
