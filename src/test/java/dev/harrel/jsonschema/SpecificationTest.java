048a4fc1223b00473ae2ac9e02fca3ef909a6d89
Harrel <marcin.slowik95@gmail.com>
2023-10-21 20:10:12 +0200

    move 2020-12 tests

--- src/test/java/dev/harrel/jsonschema/SpecificationTest.java
+++ src/test/java/dev/harrel/jsonschema/SpecificationTest.java
@@ -18,243 +18,243 @@ public abstract class SpecificationTest {
     static final Logger logger = Logger.getLogger("SpecificationTest");
     protected static JsonNodeFactory nodeFactory;
 
-    @SuiteTest("/draft2020-12/boolean_schema.json")
+    @SuiteTest("/suite/draft2020-12/boolean_schema.json")
     void booleanSchemaTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/default.json")
+    @SuiteTest("/suite/draft2020-12/default.json")
     void defaultTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/format.json")
+    @SuiteTest("/suite/draft2020-12/format.json")
     void formatTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/content.json")
+    @SuiteTest("/suite/draft2020-12/content.json")
     void contentTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/defs.json")
+    @SuiteTest("/suite/draft2020-12/defs.json")
     void defsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
     @Disabled("$id in all places are supported")
-    @SuiteTest("/draft2020-12/unknownKeyword.json")
+    @SuiteTest("/suite/draft2020-12/unknownKeyword.json")
     void unknownKeywordTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/vocabulary.json")
+    @SuiteTest("/suite/draft2020-12/vocabulary.json")
     void vocabularyTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/id.json")
+    @SuiteTest("/suite/draft2020-12/id.json")
     void idTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/infinite-loop-detection.json")
+    @SuiteTest("/suite/draft2020-12/infinite-loop-detection.json")
     void infiniteLoopDetectionTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/if-then-else.json")
+    @SuiteTest("/suite/draft2020-12/if-then-else.json")
     void ifThenElseTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/anyOf.json")
+    @SuiteTest("/suite/draft2020-12/anyOf.json")
     void anyOfTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/allOf.json")
+    @SuiteTest("/suite/draft2020-12/allOf.json")
     void allOfTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/oneOf.json")
+    @SuiteTest("/suite/draft2020-12/oneOf.json")
     void oneOfTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/not.json")
+    @SuiteTest("/suite/draft2020-12/not.json")
     void notTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/type.json")
+    @SuiteTest("/suite/draft2020-12/type.json")
     void typeTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/const.json")
+    @SuiteTest("/suite/draft2020-12/const.json")
     void constTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/enum.json")
+    @SuiteTest("/suite/draft2020-12/enum.json")
     void enumTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/items.json")
+    @SuiteTest("/suite/draft2020-12/items.json")
     void itemsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/prefixItems.json")
+    @SuiteTest("/suite/draft2020-12/prefixItems.json")
     void prefixItemsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/maxItems.json")
+    @SuiteTest("/suite/draft2020-12/maxItems.json")
     void maxItemsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/minItems.json")
+    @SuiteTest("/suite/draft2020-12/minItems.json")
     void minItemsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/uniqueItems.json")
+    @SuiteTest("/suite/draft2020-12/uniqueItems.json")
     void uniqueItemsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/contains.json")
+    @SuiteTest("/suite/draft2020-12/contains.json")
     void containsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/maxContains.json")
+    @SuiteTest("/suite/draft2020-12/maxContains.json")
     void maxContainsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/minContains.json")
+    @SuiteTest("/suite/draft2020-12/minContains.json")
     void minContainsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/unevaluatedItems.json")
+    @SuiteTest("/suite/draft2020-12/unevaluatedItems.json")
     void unevaluatedItemsTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/properties.json")
+    @SuiteTest("/suite/draft2020-12/properties.json")
     void propertiesTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/maxProperties.json")
+    @SuiteTest("/suite/draft2020-12/maxProperties.json")
     void maxPropertiesTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/additionalProperties.json")
+    @SuiteTest("/suite/draft2020-12/additionalProperties.json")
     void additionalPropertiesTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/patternProperties.json")
+    @SuiteTest("/suite/draft2020-12/patternProperties.json")
     void patternPropertiesTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/propertyNames.json")
+    @SuiteTest("/suite/draft2020-12/propertyNames.json")
     void propertyNamesTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/unevaluatedProperties.json")
+    @SuiteTest("/suite/draft2020-12/unevaluatedProperties.json")
     void unevaluatedPropertiesTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/minProperties.json")
+    @SuiteTest("/suite/draft2020-12/minProperties.json")
     void minPropertiesTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/required.json")
+    @SuiteTest("/suite/draft2020-12/required.json")
     void requiredTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/dependentRequired.json")
+    @SuiteTest("/suite/draft2020-12/dependentRequired.json")
     void dependentRequiredTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/dependentSchemas.json")
+    @SuiteTest("/suite/draft2020-12/dependentSchemas.json")
     void dependentSchemasTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/ref.json")
+    @SuiteTest("/suite/draft2020-12/ref.json")
     void refTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/refRemote.json")
+    @SuiteTest("/suite/draft2020-12/refRemote.json")
     void refRemoteTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/anchor.json")
+    @SuiteTest("/suite/draft2020-12/anchor.json")
     void anchorTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/dynamicRef.json")
+    @SuiteTest("/suite/draft2020-12/dynamicRef.json")
     void dynamicRefTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/pattern.json")
+    @SuiteTest("/suite/draft2020-12/pattern.json")
     void patternTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/maxLength.json")
+    @SuiteTest("/suite/draft2020-12/maxLength.json")
     void maxLengthTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/minLength.json")
+    @SuiteTest("/suite/draft2020-12/minLength.json")
     void minLengthTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/maximum.json")
+    @SuiteTest("/suite/draft2020-12/maximum.json")
     void maximumTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/minimum.json")
+    @SuiteTest("/suite/draft2020-12/minimum.json")
     void minimumTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/exclusiveMaximum.json")
+    @SuiteTest("/suite/draft2020-12/exclusiveMaximum.json")
     void exclusiveMaximumTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/exclusiveMinimum.json")
+    @SuiteTest("/suite/draft2020-12/exclusiveMinimum.json")
     void exclusiveMinimumTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
 
-    @SuiteTest("/draft2020-12/multipleOf.json")
+    @SuiteTest("/suite/draft2020-12/multipleOf.json")
     void multipleOfTest(String bundle, String name, JsonNode schema, JsonNode json, boolean valid) {
         testValidation(bundle, name, schema, json, valid);
     }
