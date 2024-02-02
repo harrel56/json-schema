347f83cc246e1df29d03f84e8531e3969eaa91b9
Harrel <marcin.slowik95@gmail.com>
2024-02-02 15:36:39 +0100

    initial setup for snake yaml provider

---
+++ src/main/java/dev/harrel/jsonschema/providers/SnakeYamlNode.java
@@ -0,0 +1,141 @@
+package dev.harrel.jsonschema.providers;
+
+import dev.harrel.jsonschema.JsonNode;
+import dev.harrel.jsonschema.SimpleType;
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.AbstractConstruct;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+import org.yaml.snakeyaml.nodes.Tag;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.*;
+
+public final class SnakeYamlNode implements JsonNode {
+    private final Factory factory;
+    private final Object node;
+    private final String jsonPointer;
+    private final SimpleType nodeType;
+
+    private SnakeYamlNode(Factory factory, Object node, String jsonPointer) {
+        this.factory = Objects.requireNonNull(factory);
+        this.node = node;
+        this.jsonPointer = Objects.requireNonNull(jsonPointer);
+        this.nodeType = factory.computeNodeType(node);
+    }
+
+    public SnakeYamlNode(Factory factory, Object node) {
+        this(factory, node, "");
+    }
+
+    @Override
+    public String getJsonPointer() {
+        return jsonPointer;
+    }
+
+    @Override
+    public SimpleType getNodeType() {
+        return nodeType;
+    }
+
+    @Override
+    public boolean asBoolean() {
+        return (Boolean) node;
+    }
+
+    @Override
+    public String asString() {
+        return String.valueOf(node);
+    }
+
+    @Override
+    public BigInteger asInteger() {
+        if (node instanceof BigInteger) {
+            return (BigInteger) node;
+        } else if (node instanceof BigDecimal) {
+            return ((BigDecimal) node).toBigInteger();
+        } else {
+            return BigInteger.valueOf(((Number) node).longValue());
+        }
+    }
+
+    @Override
+    public BigDecimal asNumber() {
+        if (node instanceof BigDecimal) {
+            return (BigDecimal) node;
+        } else if (node instanceof BigInteger) {
+            return new BigDecimal((BigInteger) node);
+        } else if (node instanceof Double) {
+            return BigDecimal.valueOf((Double) node);
+        } else {
+            return BigDecimal.valueOf(((Number) node).longValue());
+        }
+    }
+
+    @Override
+    public List<JsonNode> asArray() {
+        List<JsonNode> elements = new ArrayList<>();
+        for (Object o : (List<?>) node) {
+            elements.add(new SnakeYamlNode(factory, o, jsonPointer + "/" + elements.size()));
+        }
+        return elements;
+    }
+
+    @Override
+    public Map<String, JsonNode> asObject() {
+        Map<String, JsonNode> map = new HashMap<>();
+        for (Map.Entry<String, ?> entry : ((Map<String, ?>) node).entrySet()) {
+            map.put(entry.getKey(), new SnakeYamlNode(factory, entry.getValue(), jsonPointer + "/" + JsonNode.encodeJsonPointer(entry.getKey())));
+        }
+        return map;
+    }
+
+    public static final class Factory extends SimpleJsonNodeFactory {
+        @Override
+        public JsonNode wrap(Object node) {
+            if (isLiteral(node) || isArray(node) || isObject(node)) {
+                return new SnakeYamlNode(this, node);
+            } else if (node instanceof SnakeYamlNode) {
+                return (SnakeYamlNode) node;
+            } else {
+                throw new IllegalArgumentException("Cannot wrap object which is not an instance of org.json.JSONObject, org.json.JSONArray or simple literal");
+            }
+        }
+
+        @Override
+        public JsonNode create(String rawJson) {
+            class CustomConstructor extends SafeConstructor {
+                public CustomConstructor() {
+                    super(new LoaderOptions());
+                    AbstractConstruct constr = new AbstractConstruct() {
+                        @Override
+                        public Object construct(Node node) {
+                            ScalarNode scalar = (ScalarNode) node;
+                            return new BigDecimal(scalar.getValue());
+                        }
+                    };
+                    this.yamlConstructors.put(Tag.FLOAT, constr);
+                }
+            }
+            return new SnakeYamlNode(this, new Yaml(new CustomConstructor()).load(rawJson));
+        }
+
+        @Override
+        boolean isNull(Object node) {
+            return node == null;
+        }
+
+        @Override
+        boolean isArray(Object node) {
+            return node instanceof List;
+        }
+
+        @Override
+        boolean isObject(Object node) {
+            return node instanceof Map;
+        }
+    }
+}
---
+++ src/test/java/dev/harrel/jsonschema/providers/SnakeYamlTest.java
@@ -0,0 +1,136 @@
+package dev.harrel.jsonschema.providers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.harrel.jsonschema.JsonNode;
+import dev.harrel.jsonschema.JsonNodeFactory;
+import dev.harrel.jsonschema.SimpleType;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.parser.ParserException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SnakeYamlTest {
+    private JsonNodeFactory createFactory() {
+        return new SnakeYamlNode.Factory();
+    }
+
+    @Test
+    void shouldWrapForValidArgument() {
+        Map<String, Object> object = new HashMap<>();
+        JsonNode wrap = createFactory().wrap(object);
+        assertThat(wrap).isNotNull();
+        assertThat(wrap.getNodeType()).isEqualTo(SimpleType.OBJECT);
+    }
+
+    @Test
+    void shouldWrapForJsonNode() {
+        JsonNodeFactory factory = createFactory();
+        var jsonNode = factory.wrap(new HashMap<>());
+        JsonNode wrap = factory.wrap(jsonNode);
+        assertThat(wrap).isNotNull();
+        assertThat(wrap.getNodeType()).isEqualTo(SimpleType.OBJECT);
+    }
+
+    @Test
+    void shouldFailWrapForInvalidArgument() throws JsonProcessingException {
+        com.fasterxml.jackson.databind.JsonNode object = new ObjectMapper().readTree("{}");
+        JsonNodeFactory factory = createFactory();
+        assertThatThrownBy(() -> factory.wrap(object))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldFailCreateForInvalidArgument() {
+        JsonNodeFactory factory = createFactory();
+        assertThatThrownBy(() -> factory.create("{"))
+                .isInstanceOf(ParserException.class);
+    }
+
+    @Nested
+    class SpecificationSuiteTest extends dev.harrel.jsonschema.SpecificationSuiteTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class SupplementarySuiteTest extends dev.harrel.jsonschema.SupplementarySuiteTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class Draft2020EvaluatorFactoryTest extends dev.harrel.jsonschema.Draft2020EvaluatorFactoryTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class Draft2019EvaluatorFactoryTest extends dev.harrel.jsonschema.Draft2019EvaluatorFactoryTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class JsonNodeTest extends dev.harrel.jsonschema.JsonNodeTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class MetaSchemaTest extends dev.harrel.jsonschema.MetaSchemaTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class VocabulariesTest extends dev.harrel.jsonschema.VocabulariesTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class JsonPointerEscapingTest extends dev.harrel.jsonschema.JsonPointerEscapingTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class EvaluationPathTest extends dev.harrel.jsonschema.EvaluationPathTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+
+    @Nested
+    class DisabledSchemaValidationTest extends dev.harrel.jsonschema.DisabledSchemaValidationTest {
+        @Override
+        public JsonNodeFactory getJsonNodeFactory() {
+            return createFactory();
+        }
+    }
+}
