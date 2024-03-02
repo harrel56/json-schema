1cd0865261b7eade70221af7e768d7fb1d617343
unknown <mike.ennen@gmail.com>
2024-03-01 18:07:58 -0700

    Add provider implementation for jackson-jr (using jr-objects).

---
+++ src/main/java/dev/harrel/jsonschema/providers/JacksonJrNode.java
@@ -0,0 +1,90 @@
+package dev.harrel.jsonschema.providers;
+
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.jr.ob.JSON;
+import dev.harrel.jsonschema.JsonNode;
+import dev.harrel.jsonschema.JsonNodeFactory;
+
+import java.io.IOException;
+import java.util.*;
+
+public final class JacksonJrNode extends SimpleJsonNode {
+
+    private JacksonJrNode(Object node, String jsonPointer) {
+        super(Objects.requireNonNull(node), jsonPointer);
+    }
+
+    public JacksonJrNode(Object node) {
+        this(node, "");
+    }
+
+    @Override
+    public boolean asBoolean() {
+        return (Boolean) node;
+    }
+
+    @Override
+    public String asString() {
+        return Objects.toString(isNull() ? null : node);
+    }
+
+    @Override
+    List<JsonNode> createArray() {
+        ArrayNode arrayNode = (ArrayNode) node;
+        List<JsonNode> elements = new ArrayList<>();
+        for (Iterator<com.fasterxml.jackson.databind.JsonNode> it = arrayNode.elements(); it.hasNext(); ) {
+            com.fasterxml.jackson.databind.JsonNode jsonNode = it.next();
+            elements.add(new JacksonJrNode(jsonNode, jsonPointer + "/" + elements.size()));
+        }
+        return elements;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    Map<String, JsonNode> createObject() {
+        TreeNode jsonObject = (TreeNode) node;
+        Map<String, JsonNode> map = MapUtil.newHashMap(jsonObject.size());
+        for (Iterator<String> it = jsonObject.fieldNames(); it.hasNext(); ) {
+            String fieldName = it.next();
+            map.put(fieldName, new JacksonJrNode(jsonObject.get(fieldName), jsonPointer + "/" + JsonNode.encodeJsonPointer(fieldName)));
+        }
+        return map;
+    }
+
+    @Override
+    boolean isNull(Object node) {
+        System.out.println("node class: " + node.getClass());
+        return false;
+    }
+
+    @Override
+    boolean isArray(Object node) {
+        return node instanceof ArrayNode;
+    }
+
+    @Override
+    boolean isObject(Object node) {
+        return node instanceof TreeNode;
+    }
+
+    public static final class Factory implements JsonNodeFactory {
+        @Override
+        public JsonNode wrap(Object node) {
+            if (node instanceof JacksonJrNode) {
+                return (JacksonJrNode) node;
+            } else {
+                return new JacksonJrNode(node);
+            }
+        }
+
+        @Override
+        public JsonNode create(String rawJson) {
+            try {
+                return new JacksonJrNode(JSON.std.anyFrom(rawJson));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+}
---
+++ src/provider-test/jacksonJr/IsolatedGsonTest.java
@@ -0,0 +1,26 @@
+import dev.harrel.jsonschema.ValidatorFactory;
+import dev.harrel.jsonschema.providers.GsonNode;
+import dev.harrel.jsonschema.providers.JacksonJrNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class IsolatedJacksonJrTest {
+    @Test
+    void shouldInstantiateValidatorFactory() {
+        new ValidatorFactory();
+    }
+
+    @Test
+    void shouldPassForJacksonJrFactory() {
+        new ValidatorFactory()
+                .withJsonNodeFactory(new JacksonJrNode.Factory())
+                .validate("{}", "{}");
+    }
+
+    @Test
+    void shouldFailForDefaultFactory() {
+        assertThatThrownBy(() -> new ValidatorFactory().validate("{}", "{}"))
+                .isInstanceOf(NoClassDefFoundError.class);
+    }
+}
