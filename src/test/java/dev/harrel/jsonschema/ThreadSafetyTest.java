1113d123696606f73381123626f53c8f7b353fca
Harrel <marcin.slowik95@gmail.com>
2024-05-01 11:19:47 +0200

    add more tests

--- src/test/java/dev/harrel/jsonschema/ThreadSafetyTest.java
+++ src/test/java/dev/harrel/jsonschema/ThreadSafetyTest.java
@@ -1,11 +1,12 @@
 package dev.harrel.jsonschema;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.RepeatedTest;
+import dev.harrel.jsonschema.providers.JacksonNode;
+import dev.harrel.jsonschema.util.TestUtil;
+import org.junit.jupiter.api.*;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.stream.IntStream;
@@ -25,54 +26,134 @@ class ThreadSafetyTest {
         threadPool.shutdownNow();
     }
 
-    @AfterEach
-    void tearDown() {
-//        threadPool.shutdownNow();
+    @Test
+    void concurrentDirectRegistrations() throws InterruptedException, ExecutionException {
+        Validator validator = new ValidatorFactory().createValidator();
+        List<URI> uris = IntStream.range(0, 1000)
+                .mapToObj(i -> URI.create("urn:" + i))
+                .toList();
+        List<Callable<URI>> callables = uris.stream()
+                .map(uri -> (Callable<URI>) () -> validator.registerSchema(uri, "{}"))
+                .toList();
+        completeAll(callables);
+
+        for (URI uri : uris) {
+            Validator.Result result = validator.validate(uri, "{}");
+            assertThat(result.isValid()).isTrue();
+        }
     }
 
-    @RepeatedTest(100)
-    void threadSafetyRegistrationTest() throws InterruptedException, ExecutionException {
+    @Test
+    void concurrentRefResolutions() throws InterruptedException, ExecutionException {
         String schema = """
                 {
-                  "$schema": "urn:meta"
+                  "$ref": "urn:ref"
                 }""";
         Validator validator = new ValidatorFactory()
-                .withSchemaResolver(uri -> SchemaResolver.Result.fromString("true"))
+                .withDisabledSchemaValidation(true)
+                .withSchemaResolver(new LatchedSchemaResolver(100))
                 .createValidator();
+        URI uri = validator.registerSchema(schema);
+        List<Callable<Object>> callables = IntStream.range(0, 100)
+                .mapToObj(i -> (Callable<Object>) () -> validator.validate(uri, "{}"))
+                .toList();
+        completeAll(callables);
 
-            List<Callable<URI>> callables = IntStream.range(0, 1000)
-                    .mapToObj(i -> URI.create("urn:" + i))
-                    .map(uri -> (Callable<URI>) () -> validator.registerSchema(uri, schema))
-                    .toList();
-        List<Future<URI>> futures = threadPool.invokeAll(callables);
-        for (Future<URI> future : futures) {
+        Validator.Result result = validator.validate(uri, "{}");
+        assertThat(result.isValid()).isTrue();
+    }
+
+    @Test
+    void concurrentRootResolutions() throws InterruptedException, ExecutionException {
+        Validator validator = new ValidatorFactory()
+                .withDisabledSchemaValidation(true)
+                .withSchemaResolver(new LatchedSchemaResolver(100))
+                .createValidator();
+        List<URI> uris = IntStream.range(0, 100)
+                .mapToObj(i -> URI.create("urn:" + i))
+                .toList();
+        List<Callable<Object>> callables = uris.stream()
+                .map(uri -> (Callable<Object>) () -> validator.validate(uri, "{}"))
+                .toList();
+        completeAll(callables);
+
+        for (URI uri : uris) {
+            Validator.Result result = validator.validate(uri, "{}");
+            assertThat(result.isValid()).isTrue();
+        }
+    }
+
+    @Test
+    void concurrentMetaSchemaResolutions() throws InterruptedException, ExecutionException {
+        String schema = """
+                {
+                  "$schema": "urn:ref"
+                }""";
+        Validator validator = new ValidatorFactory()
+                .withSchemaResolver(new LatchedSchemaResolver(100))
+                .createValidator();
+        List<URI> uris = IntStream.range(0, 100)
+                .mapToObj(i -> URI.create("urn:" + i))
+                .toList();
+        List<Callable<URI>> callables = uris.stream()
+                .map(uri -> (Callable<URI>) () -> validator.registerSchema(uri, schema))
+                .toList();
+        completeAll(callables);
+
+        for (URI uri : uris) {
+            Validator.Result result = validator.validate(uri, "{}");
+            assertThat(result.isValid()).isTrue();
+        }
+    }
+
+    @Test
+    void concurrentRegistersWithValidates() throws InterruptedException, ExecutionException {
+        Validator validator = new ValidatorFactory()
+                .withDisabledSchemaValidation(true)
+                .withSchemaResolver(new LatchedSchemaResolver(1))
+                .createValidator();
+        URI validationUri = validator.registerSchema("{}");
+        List<Callable<Object>> writes = IntStream.range(0, 20)
+                .mapToObj(i -> URI.create("urn:" + i))
+                .map(uri -> (Callable<Object>) () -> validator.validate(uri, "{}"))
+                .toList();
+        List<Callable<Object>> reads = IntStream.range(0, 80)
+                .mapToObj(i -> (Callable<Object>) () -> validator.validate(validationUri, "{}"))
+                .toList();
+        List<Callable<Object>> callables = new ArrayList<>(writes);
+        callables.addAll(reads);
+        Collections.shuffle(callables);
+        completeAll(callables);
+    }
+
+    private static <T> void completeAll(List<Callable<T>> callables) throws ExecutionException, InterruptedException {
+        List<Future<T>> futures = threadPool.invokeAll(callables);
+        for (Future<T> future : futures) {
             future.get();
         }
-//        IntStream.range(0, 1000).parallel().forEach(i -> validator.registerSchema(schema));
-            URI uri = validator.registerSchema(schema);
-            assertThat(validator.validate(uri, "true").isValid()).isTrue();
     }
 
-    @RepeatedTest(5)
-    void threadSafetyRegistrationTest2() {
-        String schema = """
-                {
-                  "$ref": "urn:any"
-                }""";
-        Validator validator = new ValidatorFactory()
-                .withSchemaResolver(uri -> {
-                    try {
-                        System.out.println(uri);
-                        Thread.sleep(400);
-                        return SchemaResolver.Result.fromString("true");
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .createValidator();
+    private static class LatchedSchemaResolver implements SchemaResolver {
+        private final CountDownLatch latch;
+        private final JsonNode schemaNode;
 
-        URI uri = validator.registerSchema(schema);
-        IntStream.range(0, 100).parallel().forEach(i -> validator.validate(uri, "{}"));
-        assertThat(validator.validate(uri, "true").isValid()).isTrue();
+        LatchedSchemaResolver(int latchSize) {
+            this.latch = new CountDownLatch(latchSize);
+            String schemaString = TestUtil.readResource(SpecificationVersion.DRAFT2020_12.getResourcePath());
+            this.schemaNode = new JacksonNode.Factory().create(schemaString);
+        }
+
+        @Override
+        public Result resolve(String uri) {
+            try {
+                latch.countDown();
+                if (!latch.await(2, TimeUnit.SECONDS)) {
+                    throw new IllegalStateException("Latch timeout reached");
+                }
+                return Result.fromJsonNode(schemaNode);
+            } catch (InterruptedException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
     }
 }
