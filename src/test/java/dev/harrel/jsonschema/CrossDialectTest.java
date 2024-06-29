8b66270a12279d39b457ae1cbf7380f507dfe690
Harrel <marcin.slowik95@gmail.com>
2024-06-29 17:31:39 +0200

    add test for fake embedded schema

--- src/test/java/dev/harrel/jsonschema/CrossDialectTest.java
+++ src/test/java/dev/harrel/jsonschema/CrossDialectTest.java
@@ -32,4 +32,28 @@ class CrossDialectTest {
                 .validate(schema, "[1]");
         System.out.println(result);
     }
+
+    @Test
+    void compoundSchemaValidatesFakeEmbeddedSchemas() {
+        String schema = """
+                {
+                  "$schema": "https://json-schema.org/draft/2020-12/schema",
+                  "$ref": "#/$defs/draft2019",
+                  "$defs": {
+                    "draft2019": {
+                      "$schema": "https://json-schema.org/draft/2019-09/schema",
+                      "items": [{
+                        "type": "string"
+                      }]
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory().createValidator();
+        URI uri = URI.create("urn:test");
+        InvalidSchemaException exception = catchThrowableOfType(InvalidSchemaException.class, () -> validator.registerSchema(uri, schema));
+        assertThat(exception).hasMessage("Schema [urn:test] failed to validate against meta-schema [https://json-schema.org/draft/2020-12/schema]");
+        assertThat(exception.getErrors()).hasSize(11); // TBH exact error count is not so important here
+        assertThat(exception.getErrors().getFirst().getError()).isEqualTo("Value is [array] but should be [object, boolean]");
+    }
 }
