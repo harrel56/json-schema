3311f34a77ab5e8a9330206f75e98326f3b27240
Harrel <marcin.slowik95@gmail.com>
2024-06-29 17:41:34 +0200

    fake embedded schemas tests

--- src/test/java/dev/harrel/jsonschema/CompoundSchemaTest.java
+++ src/test/java/dev/harrel/jsonschema/CompoundSchemaTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 
+import static dev.harrel.jsonschema.util.TestUtil.assertError;
 import static org.assertj.core.api.Assertions.*;
 
 class CompoundSchemaTest {
@@ -54,5 +55,72 @@ class CompoundSchemaTest {
         assertThat(exception).hasMessage("Schema [urn:test] failed to validate against meta-schema [https://json-schema.org/draft/2020-12/schema]");
         assertThat(exception.getErrors()).hasSize(11); // TBH exact error count is not so important here
         assertThat(exception.getErrors().getFirst().getError()).isEqualTo("Value is [array] but should be [object, boolean]");
+    }
+
+    @Test
+    void schemaKeywordIsIgnoredForFakeEmbeddedSchemas() {
+        String schema = """
+                {
+                  "$schema": "https://json-schema.org/draft/2020-12/schema",
+                  "$id": "urn:compound",
+                  "$ref": "#/$defs/x",
+                  "$defs": {
+                    "x": {
+                      "$schema": "urn:this-resolves-to-nothing",
+                      "type": ["null"]
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory().createValidator();
+        URI uri = validator.registerSchema(schema);
+        Validator.Result result = validator.validate(uri, "{}");
+        assertThat(result.isValid()).isFalse();
+        assertThat(result.getErrors()).hasSize(1);
+        assertError(
+                result.getErrors().getFirst(),
+                "/$ref/type",
+                "urn:compound#",
+                "",
+                "type",
+                "Value is [object] but should be [null]"
+        );
+    }
+
+    @Test
+    void fakeEmbeddedSchemasAreNotValidated() {
+        String failingSchema = """
+                {
+                  "$schema": "https://json-schema.org/draft/2020-12/schema",
+                  "$id": "urn:failing",
+                  "type": "string"
+                }""";
+        String schema = """
+                {
+                  "$schema": "https://json-schema.org/draft/2020-12/schema",
+                  "$id": "urn:compound",
+                  "$ref": "#/$defs/x",
+                  "$defs": {
+                    "x": {
+                      "$schema": "urn:failing",
+                      "type": ["null"]
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory().createValidator();
+        validator.registerSchema(failingSchema);
+        URI uri = validator.registerSchema(schema);
+        Validator.Result result = validator.validate(uri, "{}");
+        assertThat(result.isValid()).isFalse();
+        assertThat(result.getErrors()).hasSize(1);
+        assertError(
+                result.getErrors().getFirst(),
+                "/$ref/type",
+                "urn:compound#",
+                "",
+                "type",
+                "Value is [object] but should be [null]"
+        );
     }
 }
