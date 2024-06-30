2f871a1d71c6ea5ef84911f1ac8f1191b3af681c
Harrel <marcin.slowik95@gmail.com>
2024-06-30 14:47:38 +0200

    add some dialect deprecations

--- src/main/java/dev/harrel/jsonschema/Dialect.java
+++ src/main/java/dev/harrel/jsonschema/Dialect.java
@@ -34,23 +34,41 @@ public interface Dialect {
      * All vocabulary URIs that are supported by this dialect. Actual implementation for vocabularies and keywords
      * should be provided by {@link Dialect#getEvaluatorFactory()}
      *
+     * @deprecated since version 1.7.0. As handling dialects and spec versions has changed this method is no longer used.
+     * Implementing it has no effect.
+     *
      * @return set of supported vocabularies
      */
-    Set<String> getSupportedVocabularies();
+    @Deprecated
+    default Set<String> getSupportedVocabularies() {
+        throw new UnsupportedOperationException();
+    }
 
     /**
      * All vocabulary URIs that are considered required at all times. If <i>$vocabulary</i> keyword
      * does not include this vocabulary or sets it as optional, {@link VocabularyException} will be thrown.
      *
+     * @deprecated since version 1.7.0. As handling dialects and spec versions has changed this method is no longer used.
+     * Implementing it has no effect.
+     *
      * @return set of required vocabularies
      */
-    Set<String> getRequiredVocabularies();
+    @Deprecated
+    default Set<String> getRequiredVocabularies() {
+        throw new UnsupportedOperationException();
+    }
 
     /**
      * Default <i>$vocabulary</i> keyword contents for this dialect. If meta-schema does not include
      * <i>$vocabulary</i> keyword this default will be used.
      *
+     * @deprecated since version 1.7.0. As handling dialects and spec versions has changed this method is no longer used.
+     * Implementing it has no effect.
+     *
      * @return default vocabulary object
      */
-    Map<String, Boolean> getDefaultVocabularyObject();
+    @Deprecated
+    default Map<String, Boolean> getDefaultVocabularyObject() {
+        throw new UnsupportedOperationException();
+    }
 }
