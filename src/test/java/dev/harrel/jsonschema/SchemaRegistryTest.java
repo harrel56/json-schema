2218e22b81616fd1f0fd9ff4fee045dfcb9f7c6d
Harrel <marcin.slowik95@gmail.com>
2024-07-09 21:44:51 +0200

    uncomment SchemaRegistryTest

--- src/test/java/dev/harrel/jsonschema/SchemaRegistryTest.java
+++ src/test/java/dev/harrel/jsonschema/SchemaRegistryTest.java
@@ -12,33 +12,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SchemaRegistryTest {
 
-//    @Test
-//    void shouldRestoreStateProperly() {
-//        SchemaRegistry schemaRegistry = new SchemaRegistry();
-//        SchemaParsingContext ctx = new SchemaParsingContext(new Dialects.Draft2020Dialect(), schemaRegistry, URI.create("urn:test"), emptyMap());
-//        JacksonNode.Factory factory = new JacksonNode.Factory();
-//        JacksonNode rootSchemaNode = factory.create("""
-//                {
-//                  "properties": {
-//                    "field": true
-//                  }
-//                }""");
-//        JsonNode subSchemaNode = rootSchemaNode.asObject().get("properties").asObject().get("field");
-//
-//        schemaRegistry.registerSchema(ctx, subSchemaNode, List.of(), Set.of());
-//        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#/properties/field"))).isNotNull();
-//
-//        SchemaRegistry.State snapshot = schemaRegistry.createSnapshot();
-//        schemaRegistry.registerSchema(ctx, rootSchemaNode, List.of(), Set.of());
-//        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#"))).isNotNull();
-//        SchemaRegistry.State nextSnapshot = schemaRegistry.createSnapshot();
-//
-//        schemaRegistry.restoreSnapshot(snapshot);
-//        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#/properties/field"))).isNotNull();
-//        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#"))).isNull();
-//
-//        schemaRegistry.restoreSnapshot(nextSnapshot);
-//        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#/properties/field"))).isNotNull();
-//        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#"))).isNotNull();
-//    }
+    @Test
+    void shouldRestoreStateProperly() {
+        SchemaRegistry schemaRegistry = new SchemaRegistry();
+        SchemaParsingContext ctx = new SchemaParsingContext(new MetaSchemaData(new Dialects.Draft2020Dialect()), schemaRegistry, URI.create("urn:test"), emptyMap());
+        JacksonNode.Factory factory = new JacksonNode.Factory();
+        JacksonNode rootSchemaNode = factory.create("""
+                {
+                  "properties": {
+                    "field": true
+                  }
+                }""");
+        JsonNode subSchemaNode = rootSchemaNode.asObject().get("properties").asObject().get("field");
+
+        schemaRegistry.registerSchema(ctx, subSchemaNode, List.of());
+        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#/properties/field"))).isNotNull();
+
+        SchemaRegistry.State snapshot = schemaRegistry.createSnapshot();
+        schemaRegistry.registerSchema(ctx, rootSchemaNode, List.of());
+        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#"))).isNotNull();
+        SchemaRegistry.State nextSnapshot = schemaRegistry.createSnapshot();
+
+        schemaRegistry.restoreSnapshot(snapshot);
+        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#/properties/field"))).isNotNull();
+        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#"))).isNull();
+
+        schemaRegistry.restoreSnapshot(nextSnapshot);
+        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#/properties/field"))).isNotNull();
+        assertThat(schemaRegistry.get(CompoundUri.fromString("urn:test#"))).isNotNull();
+    }
 }
