a3aafaf02a3dd9d3aae639409025c456bac8d727
Harrel <marcin.slowik95@gmail.com>
2024-07-10 19:50:39 +0200

    unused import

--- src/test/java/dev/harrel/jsonschema/ThreadSafetyTest.java
+++ src/test/java/dev/harrel/jsonschema/ThreadSafetyTest.java
@@ -8,7 +8,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
