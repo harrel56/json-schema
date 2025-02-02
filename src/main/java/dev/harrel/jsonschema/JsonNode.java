6166ba7886a8ab8575a8295071ba3581056ed712
Harrel <marcin.slowik95@gmail.com>
2025-02-02 20:58:07 +0100

    cleanup JsonNode, add javadoc

--- src/main/java/dev/harrel/jsonschema/JsonNode.java
+++ src/main/java/dev/harrel/jsonschema/JsonNode.java
@@ -5,11 +5,10 @@ import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
-import static dev.harrel.jsonschema.ComparatorHelper.*;
-import static dev.harrel.jsonschema.ComparatorHelper.compareObjects;
-
 /**
  * {@code JsonNode} interface is the main abstraction for provider-agnostic JSON node.
+ * Implementations are required to implement {@link Object#equals(Object)} and {@link Object#hashCode()}.
+ * Equality check must follow JSON equality rules, e.g. JSON properties order is not relevant.
  */
 public interface JsonNode {
     /**
@@ -119,58 +118,5 @@ public interface JsonNode {
         } else {
             return asString();
         }
-    }
-
-    /**
-     * Equality check between two {@link JsonNode}s.
-     * Must follow JSON equality rules, e.g. JSON properties order is not relevant.
-     */
-    default boolean isEqualTo(JsonNode other) {
-        if (isNull() && other.isNull()) {
-            return true;
-        } else if (isBoolean() && other.isBoolean()) {
-            return asBoolean() == other.asBoolean();
-        } else if (isString() && other.isString()) {
-            return asString().equals(other.asString());
-        } else if (isInteger() && other.isInteger()) {
-            return asInteger().equals(other.asInteger());
-        } else if (isNumber() && other.isNumber()) {
-            return asNumber().equals(other.asNumber());
-        } else if (isArray() && other.isArray()) {
-            return compareArrays(asArray(), other.asArray());
-        } else if (isObject() && other.isObject()) {
-            return compareObjects(asObject(), other.asObject());
-        } else {
-            return false;
-        }
-    }
-}
-
-class ComparatorHelper {
-    private ComparatorHelper() {}
-
-    static boolean compareArrays(List<JsonNode> arr1, List<JsonNode> arr2) {
-        if (arr1.size() != arr2.size()) {
-            return false;
-        }
-        for (int i = 0; i < arr1.size(); i++) {
-            if (!arr1.get(i).isEqualTo(arr2.get(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    static boolean compareObjects(Map<String, JsonNode> object1, Map<String, JsonNode> object2) {
-        if (object1.size() != object2.size()) {
-            return false;
-        }
-        for (Map.Entry<String, JsonNode> entry : object1.entrySet()) {
-            JsonNode otherField = object2.get(entry.getKey());
-            if (otherField == null || !entry.getValue().isEqualTo(otherField)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
