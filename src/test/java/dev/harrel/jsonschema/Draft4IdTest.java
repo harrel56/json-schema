05619f40dfab4a92463d1aff950c7f23445e5bc3
harrel <marcin.slowik95@gmail.com>
2025-02-16 19:44:38 +0100

    add more legacy id tests

--- src/test/java/dev/harrel/jsonschema/Draft4IdTest.java
+++ src/test/java/dev/harrel/jsonschema/Draft4IdTest.java
@@ -8,7 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class Draft4IdTest {
-    private static final URI SCHEMA_URI = URI.create("urn:draft4");
+    private static final URI DRAFT2020_SCHEMA = URI.create("urn:draft2020");
+    private static final URI DRAFT4_SCHEMA = URI.create("urn:draft4");
     private static final String DERIVED_DRAFT_4_META_SCHEMA = """
             {
               "$schema": "http://json-schema.org/draft-04/schema#",
@@ -31,7 +32,7 @@ class Draft4IdTest {
                 .withDefaultDialect(new Dialects.Draft4Dialect())
                 .createValidator();
         validator.registerSchema(schema);
-        Validator.Result res = validator.validate(SCHEMA_URI, "{}");
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
 
         assertThat(res.isValid()).isTrue();
     }
@@ -47,7 +48,7 @@ class Draft4IdTest {
                 .withDefaultDialect(new Dialects.Draft4Dialect())
                 .createValidator();
         validator.registerSchema(schema);
-        assertThatThrownBy(() -> validator.validate(SCHEMA_URI, "{}"))
+        assertThatThrownBy(() -> validator.validate(DRAFT4_SCHEMA, "{}"))
                 .isInstanceOf(SchemaNotFoundException.class);
     }
 
@@ -61,7 +62,7 @@ class Draft4IdTest {
 
         Validator validator = new ValidatorFactory().createValidator();
         validator.registerSchema(schema);
-        Validator.Result res = validator.validate(SCHEMA_URI, "{}");
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
 
         assertThat(res.isValid()).isTrue();
     }
@@ -77,7 +78,7 @@ class Draft4IdTest {
         Validator validator = new ValidatorFactory().createValidator();
         validator.registerSchema(DERIVED_DRAFT_4_META_SCHEMA);
         validator.registerSchema(schema);
-        Validator.Result res = validator.validate(SCHEMA_URI, "{}");
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
 
         assertThat(res.isValid()).isTrue();
     }
@@ -99,19 +100,13 @@ class Draft4IdTest {
                 })
                 .createValidator();
         validator.registerSchema(schema);
-        Validator.Result res = validator.validate(SCHEMA_URI, "{}");
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
 
         assertThat(res.isValid()).isTrue();
     }
 
     @Test
     void shouldFailForPreloadedRecursiveDraft4MetaSchema() {
-        String schema = """
-                {
-                  "$schema": "urn:recursive-draft4",
-                  "id": "urn:draft4"
-                }""";
-
         Validator validator = new ValidatorFactory().createValidator();
         /* This probably should contain more detailed message but as an edge case I guess it's fine */
         assertThatThrownBy(() -> validator.registerSchema(RECURSIVE_DRAFT_4_META_SCHEMA))
@@ -142,5 +137,211 @@ class Draft4IdTest {
                 .cause()
                 .isInstanceOf(MetaSchemaResolvingException.class)
                 .hasMessage("Parsing meta-schema [urn:recursive-draft4] failed - only dialects explicitly added to a validator can be recursive");
+    }
+
+    @Test
+    void shouldRegisterEmbeddedSchemaWithDefaultDraft4() {
+        String schema = """
+                {
+                  "definitions": {
+                    "embedded": {
+                      "id": "urn:draft4"
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory()
+                .withDefaultDialect(new Dialects.Draft4Dialect())
+                .createValidator();
+        validator.registerSchema(schema);
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
+
+        assertThat(res.isValid()).isTrue();
+    }
+
+    @Test
+    void shouldNotRegisterEmbeddedNewIdWithDefaultDraft4() {
+        String schema = """
+                {
+                  "definitions": {
+                    "embedded": {
+                      "$id": "urn:draft4"
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory()
+                .withDefaultDialect(new Dialects.Draft4Dialect())
+                .createValidator();
+        validator.registerSchema(schema);
+        assertThatThrownBy(() -> validator.validate(DRAFT4_SCHEMA, "{}"))
+                .isInstanceOf(SchemaNotFoundException.class);
+    }
+
+    @Test
+    void shouldRegisterEmbeddedSchemaWithExplicitDraft4() {
+        String schema = """
+                {
+                  "$id": "urn:draft2020",
+                  "definitions": {
+                    "embedded": {
+                      "$schema": "http://json-schema.org/draft-04/schema#",
+                      "id": "urn:draft4",
+                      "const": null
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory().createValidator();
+        validator.registerSchema(schema);
+
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
+        assertThat(res.isValid()).isTrue();
+        res = validator.validate(DRAFT2020_SCHEMA, "{}");
+        assertThat(res.isValid()).isTrue();
+    }
+
+    @Test
+    void shouldNotRegisterEmbeddedNewIdWithExplicitDraft4() {
+        String schema = """
+                {
+                  "id": "urn:draft2020",
+                  "definitions": {
+                    "embedded": {
+                      "$schema": "http://json-schema.org/draft-04/schema#",
+                      "$id": "urn:draft4"
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory().createValidator();
+        validator.registerSchema(schema);
+        assertThatThrownBy(() -> validator.validate(DRAFT4_SCHEMA, "{}"))
+                .isInstanceOf(SchemaNotFoundException.class);
+        assertThatThrownBy(() -> validator.validate(DRAFT2020_SCHEMA, "{}"))
+                .isInstanceOf(SchemaNotFoundException.class);
+    }
+
+    /**
+     * This is edge case when it doesn't work perfectly:
+     * If embedded $schema is not recognized as registered dialect
+     * then we fall back to a default dialect, so in most cases to $id usage
+     */
+    @Test
+    void shouldNotRegisterEmbeddedSchemaWithDerivedDraft4MetaSchema() {
+        String schema = """
+                {
+                  "id": "urn:draft2020",
+                  "definitions": {
+                    "embedded": {
+                      "$schema": "urn:derived-draft4",
+                      "id": "urn:draft4"
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory().createValidator();
+        validator.registerSchema(DERIVED_DRAFT_4_META_SCHEMA);
+        validator.registerSchema(schema);
+        assertThatThrownBy(() -> validator.validate(DRAFT4_SCHEMA, "{}"))
+                .isInstanceOf(SchemaNotFoundException.class);
+        assertThatThrownBy(() -> validator.validate(DRAFT2020_SCHEMA, "{}"))
+                .isInstanceOf(SchemaNotFoundException.class);
+    }
+
+    /**
+     * This is edge case when it doesn't work perfectly:
+     * If embedded $schema is not recognized as registered dialect
+     * then we fall back to a default dialect, so in most cases to $id usage
+     */
+    @Test
+    void shouldRegisterEmbeddedNewIdWithDerivedDraft4MetaSchema() {
+        String schema = """
+                {
+                  "$id": "urn:draft2020",
+                  "definitions": {
+                    "embedded": {
+                      "$schema": "urn:derived-draft4",
+                      "$id": "urn:draft4",
+                      "const": null
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory().createValidator();
+        validator.registerSchema(DERIVED_DRAFT_4_META_SCHEMA);
+        validator.registerSchema(schema);
+
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
+        assertThat(res.isValid()).isTrue();
+        res = validator.validate(DRAFT2020_SCHEMA, "{}");
+        assertThat(res.isValid()).isTrue();
+    }
+
+    @Test
+    void shouldRegisterEmbeddedSchemaWithExplicitDraft2020() {
+        String schema = """
+                {
+                  "id": "urn:draft4",
+                  "definitions": {
+                    "embedded": {
+                      "$schema": "https://json-schema.org/draft/2020-12/schema",
+                      "$id": "urn:draft2020",
+                      "dependencies": {
+                        "fail": {
+                          "not": {}
+                        }
+                      }
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory()
+                .withDefaultDialect(new Dialects.Draft4Dialect())
+                .createValidator();
+        validator.registerSchema(schema);
+
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
+        assertThat(res.isValid()).isTrue();
+        res = validator.validate(DRAFT2020_SCHEMA, "{\"fail\": {}}");
+        assertThat(res.isValid()).isTrue();
+    }
+
+    /**
+     * Again, $id/id mismatch
+     */
+    @Test
+    void shouldRegisterEmbeddedSchemaWithDerivedDraft2020MetaSchema() {
+        String metaSchema = """
+                {
+                  "$schema": "https://json-schema.org/draft/2020-12/schema",
+                  "$id": "urn:derived-draft2020"
+                }""";
+        String schema = """
+                {
+                  "id": "urn:draft4",
+                  "definitions": {
+                    "embedded": {
+                      "$schema": "urn:derived-draft2020",
+                      "id": "urn:draft2020",
+                      "dependencies": {
+                        "fail": {
+                          "not": {}
+                        }
+                      }
+                    }
+                  }
+                }""";
+
+        Validator validator = new ValidatorFactory()
+                .withDefaultDialect(new Dialects.Draft4Dialect())
+                .createValidator();
+        validator.registerSchema(metaSchema);
+        validator.registerSchema(schema);
+
+        Validator.Result res = validator.validate(DRAFT4_SCHEMA, "{}");
+        assertThat(res.isValid()).isTrue();
+        res = validator.validate(DRAFT2020_SCHEMA, "{\"fail\": {}}");
+        assertThat(res.isValid()).isTrue();
     }
 }
