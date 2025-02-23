6366865d8a95f05a8691c2958883cd79714dd03a
Harrel <marcin.slowik95@gmail.com>
2025-02-23 13:17:12 +0100

    bring back old isEqualTo in form of a static method

--- src/main/java/dev/harrel/jsonschema/JsonNodeUtil.java
+++ src/main/java/dev/harrel/jsonschema/JsonNodeUtil.java
@@ -1,6 +1,7 @@
 package dev.harrel.jsonschema;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -36,5 +37,54 @@ final class JsonNodeUtil {
                         .filter(entry -> entry.getValue().isBoolean())
                         .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().asBoolean())))
                 .map(Collections::unmodifiableMap);
+    }
+
+    static boolean equals(JsonNode node1, JsonNode node2) {
+        if (node1.getNodeType() != node2.getNodeType()) {
+            return false;
+        }
+        switch (node1.getNodeType()) {
+            case NULL:
+                return true;
+            case BOOLEAN:
+                return node1.asBoolean() == node2.asBoolean();
+            case STRING:
+                return node1.asString().equals(node2.asString());
+            case INTEGER:
+                return node1.asInteger().equals(node2.asInteger());
+            case NUMBER:
+                return node1.asNumber().equals(node2.asNumber());
+            case ARRAY:
+                return compareArrays(node1.asArray(), node2.asArray());
+            case OBJECT:
+                return compareObjects(node1.asObject(), node2.asObject());
+            default:
+                throw new IllegalArgumentException(String.format("Unknown nodeType [%s]", node1.getNodeType()));
+        }
+    }
+
+    private static boolean compareArrays(List<JsonNode> arr1, List<JsonNode> arr2) {
+        if (arr1.size() != arr2.size()) {
+            return false;
+        }
+        for (int i = 0; i < arr1.size(); i++) {
+            if (!equals(arr1.get(i), arr2.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean compareObjects(Map<String, JsonNode> object1, Map<String, JsonNode> object2) {
+        if (object1.size() != object2.size()) {
+            return false;
+        }
+        for (Map.Entry<String, JsonNode> entry : object1.entrySet()) {
+            JsonNode otherField = object2.get(entry.getKey());
+            if (otherField == null || !equals(entry.getValue(), otherField)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
